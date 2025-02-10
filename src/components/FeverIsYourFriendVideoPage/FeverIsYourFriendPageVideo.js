af806a421dcbe8e8e3746ecf09f5618511759e8e
Beko-ux <bekonoonana8@gmail.com>
2025-02-10 01:04:34 +0100

    update

--- src/components/FeverIsYourFriendVideoPage/FeverIsYourFriendPageVideo.js
+++ src/components/FeverIsYourFriendVideoPage/FeverIsYourFriendPageVideo.js
@@ -3,7 +3,7 @@ const FeverIsYourFriendPageVideo = () => {
   return (
     <div className="FeverIsYourFriendPageVideo">
       <iframe
-        src="https://drive.google.com/file/d/1bEZdIk-X1E_2aajGqq5Wy3axaignE7J6/preview"
+        src="https://drive.google.com/file/d/1TbspWj1FznOeRVwDYRH1Eve_G1v0_MLe/preview"
         width="100%"
         height="280"
         allow="autoplay"
